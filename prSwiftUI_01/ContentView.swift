6c5766437618abf9879b874874fcc2169e649265
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2021-03-21 13:31:10 +0100

    Initial Commit

---
+++ prSwiftUI_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prSwiftUI_01
+//
+//  Created by Miguel on 21/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prSwiftUI_01/prSwiftUI_01App.swift
@@ -0,0 +1,17 @@
+//
+//  prSwiftUI_01App.swift
+//  prSwiftUI_01
+//
+//  Created by Miguel on 21/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct prSwiftUI_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
